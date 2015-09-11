6c5c224faff6e08c5cfd4c0ac33fb44c0c4e5081
Gregory Grubbs <gregory@dynapse.com>
2015-09-11 09:26:22 -0600

    Part 4: DB

--- microblog/config.py
+++ microblog/config.py
@@ -1,3 +1,4 @@
+import os
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 OPENID_PROVIDERS = [
@@ -6,3 +7,9 @@ OPENID_PROVIDERS = [
     {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
     {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
     {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
+# SQLAlchemy ORM
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
